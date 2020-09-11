591e1b328cc21d6a430451069d37d7cdebe64968
Arthur <ArthurWang2000@hotmail.com>
2020-09-11 18:38:00 +1000

    Setted up foundation for changing background image for each boards, made a new home page which shows a list of all the user's task boards, added links for some of the nav bar a refs

--- app.py
+++ app.py
@@ -4,11 +4,14 @@ import signal
 
 app = Flask(__name__)
 
+# Each user have their own collection of boards
+BOARDS = []
 # temp global variable, should replace with database or something 
-BOARD = Board.new_board()
+BOARD = Board.new_board("My crappy board")
+BOARDS.append(BOARD)
+BOARDS.append(Board.new_board("Test_board n2"))
 
-
-@app.route("/add_item", methods=["POST"])
+@app.route("/board/add_item", methods=["POST"])
 def add_item():
     print("WOw")
     issue_name = request.json['name']
@@ -17,42 +20,46 @@ def add_item():
     BOARD.add_item(col_id, issue_name, issue_descript)
     return {}
 
-@app.route("/add_column", methods=["POST"])
+@app.route("/board/add_column", methods=["POST"])
 def add_column():
     col_name = request.json['name']
     print(col_name)
     BOARD.add_new_column(col_name)
     return {}
 
-@app.route("/delete_issue", methods=["DELETE"])
+@app.route("/board/delete_issue", methods=["DELETE"])
 def delete_issue():
     item_id = request.json["id"]
     BOARD.remove_item(item_id)
     return {}
 
-@app.route("/delete_column", methods=["DELETE"])
+@app.route("/board/delete_column", methods=["DELETE"])
 def delete_column():
     col_id = request.json["id"]
     BOARD.remove_column(col_id)
     return {}
 
-@app.route("/move_column", methods=["PUT"])
+@app.route("/board/move_column", methods=["PUT"])
 def move_column():
     col_id = request.json["id"]
     is_left = request.json["left"]
     BOARD.move_column(col_id, is_left)
     return {}
 
-@app.route("/move_issue", methods=["PUT"])
+@app.route("/board/move_issue", methods=["PUT"])
 def move_issue():
     issue_id = request.json["id"]
     direction = request.json["direction"]
     BOARD.move_item(issue_id, direction)
     return {}
 
+@app.route("/board", methods=["GET"])
+def board():
+    return render_template('board_page.html', boards=BOARDS, board=BOARD, columns=BOARD.board)
+
 @app.route("/", methods=["GET"])
 def home():
-    return render_template('home.html', columns=BOARD.board)
+    return render_template('home.html', boards=BOARDS, board=BOARD, columns=BOARD.board)
 
 
 def sig_handler(signum, frame):
